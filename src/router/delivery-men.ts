53b2dfee5f0762d0c47e18f7ec5b2163fd7a61dd
Zayar Linn Naung <linnnaung233z@prestigein.com>
2025-08-25 19:32:00 +0630

    update

--- src/router/delivery-men.ts
+++ src/router/delivery-men.ts
@@ -1,14 +1,2 @@
 export const deliveryMenRouter = [
-  {
-    path: "/delivery-men",
-    lazy: async () => ({
-      Component: (await import("@/page/delivery-men")).default,
-    }),
-  },
-  {
-    path: "/delivery-men/add",
-    lazy: async () => ({
-      Component: (await import("@/page/delivery-men/add-delivery-men")).default,
-    }),
-  },
 ];
--- src/router/manage-service.tsx
+++ src/router/manage-service.tsx
@@ -1,20 +1,2 @@
 export const manageServiceRouter = [
-  {
-    path: "/zone",
-    lazy: async () => ({
-      Component: (await import("@/page/zone")).default,
-    }),
-  },
-  {
-    path: "/zone/create",
-    lazy: async () => ({
-      Component: (await import("@/page/zone/create-zone")).default,
-    }),
-  },
-  {
-    path: "/zone/update/:id",
-    lazy: async () => ({
-      Component: (await import("@/page/zone/update-zone")).default,
-    }),
-  },
 ];
